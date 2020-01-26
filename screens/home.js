0fba5b40ad19f639439b2fe115ae769e30f0b33a
Sumeer Tuli <sumeertuli7@gmail.com>
2020-01-26 12:00:45 -0800

    initial commit

---
+++ screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function Home() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
+    </View>
+  );
+}
